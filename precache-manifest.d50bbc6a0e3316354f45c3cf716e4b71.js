702c36caaa59da374b0b21bb215e07abb5cf4609
Dmitry Chuvykin <dmi4uv@gmail.com>
2020-05-12 15:51:51 +0400

    Updates

---
+++ precache-manifest.d50bbc6a0e3316354f45c3cf716e4b71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a09d16f20aa640674e06acf8a8efd9",
+    "url": "/saga/index.html"
+  },
+  {
+    "revision": "21675a536416f9c94cc9",
+    "url": "/saga/static/css/main.3e5919d4.chunk.css"
+  },
+  {
+    "revision": "a6d26c3f529eba3a34a6",
+    "url": "/saga/static/js/2.573dffc1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/saga/static/js/2.573dffc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21675a536416f9c94cc9",
+    "url": "/saga/static/js/main.7edfeb55.chunk.js"
+  },
+  {
+    "revision": "37ed3ef37828214110bc",
+    "url": "/saga/static/js/runtime-main.b09fdc97.js"
+  }
+]);
